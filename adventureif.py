e316c6fd268743ffda21aba965d1286a50bd989a
Kayleigh Roberts <kayleigh.ruth.roberts@gmail.com>
2019-02-18 07:10:32 +0000

    added a locked door, a lockpick, and a working open help function

--- adventureif.py
+++ adventureif.py
@@ -20,17 +20,26 @@ broken_things = [] # basket for things that need fixing
 current_action = []
 
 commands = ["add", "pickup", "take", "get",
-            "drop", "remove", "get rid of",
+            "drop", "remove",
             "go", "move", "travel", "proceed", "relocate",
             "open",
             "close", "shut", "lock", "slam",
             "smash", "bash", "destroy", "break"]
-add_inventory = ["add", "pickup", "take", "get"]
-remove_inventory = ["drop", "remove", "get rid of"]
+add_inventory = ["add", "pickup", "pick up", "take", "get"]
+remove_inventory = ["drop", "remove"]
 travel = ["go", "move", "travel", "proceed", "relocate"]
 door_open = "open"
-door_close = ["close", "shut", "lock", "slam"]
+door_close = ["close", "shut", "slam"]
 door_smash = ["smash", "bash", "destroy", "break"]
+door_lock = "lock"
+door_unlock = ["unlock", "pick"]
+
+closed_door = ["locked closed door", "unlocked closed door"]
+open_door = ["locked open door", "unlocked open door"]
+locked_door = ["locked open door", "locked closed door"]
+unlocked_door = ["unlocked open door", "unlocked closed door"]
+walkable_door = ["locked open door", "unlocked open door", "smashed door"]
+smashable_door = ["locked closed door", "unlocked closed door", "locked open door", "unlocked open door"]
 
 search_valid = {"idk": True, "i dont know": True, "i don't know": True, "options": True, "search": True, "choices": True, "help": True}
 
@@ -144,34 +153,84 @@ def stopgo_query(question, default=None):
             sys.stdout.write("> Please respond with 'yes' or 'no' "
                              "(or 'y' or 'n'). Type 'help' to see more options\n")
 
+def unlock_door_cmd():
+    if "locked open door" in surroundings:
+        surroundings.remove("locked open door")
+        surroundings.append("unlocked open door")
+    elif "locked closed door" in surroundings:
+        surroundings.remove("locked closed door")
+        surroundings.append("unlocked closed door")
+    print4 ("I've unlocked the door.")
+
+def lock_door_cmd():
+    if "unlocked open door" in surroundings:
+        surroundings.remove("unlocked open door")
+        surroundings.append("locked open door")
+    elif "unlocked closed door" in surroundings:
+        surroundings.remove("unlocked closed door")
+        surroundings.append("locked closed door")
+    print4 ("I've locked the door.")
+
+def smash_door_cmd():
+    if "unlocked closed door" in surroundings:
+        surroundings.remove("unlocked closed door")
+    elif "locked closed door" in surroundings:
+        surroundings.remove("locked closed door")
+    elif "unlocked open door" in surroundings:
+        surroundings.remove("unlocked open door")
+    elif "locked open door" in surroundings:
+        surroundings.remove("locked open door")
+    else:
+        print4 ("[[[INVALID DOOR STATE]]]")
+    surroundings.append("smashed door")
+    print4 ("I've smashed the door.")
+
 def action_seq():
     current_action.clear()
     #sys.stdout.write(question)
     userinput = input("> ").lower().strip()
     choice = userinput.split()
     objectforsurroundings = ''.join(set(choice).intersection(surroundings))
-    objectforinventory = ''.join(set(choice).intersection(inventory))
     commandfordirection = ''.join(set(choice).intersection(direction))
     # if no command
     if userinput == '':
         action_seq()
+    # help
+    elif any(elem in choice for elem in search_valid):
+        current_action.append("help")
+        if any(elem in choice for elem in add_inventory) or "pick" and "up" in choice:
+            print4 ("To pick up an object, you can say 'add', 'pickup', 'pick up', 'take', or 'get'. For example, say 'pick up the lockpick' to pick up the lockpick.")
+        elif any(elem in choice for elem in remove_inventory):
+            print4 ("To drop an object, just say 'drop' or 'remove'. You don't need to specify the object, as you can only carry one thing at a time.")
+        elif any(elem in choice for elem in travel):
+            print4 ("To go in a specific direction, say 'go', 'move', or 'travel' followed by a direction. You can also travel to a specific thing, so you can say 'go sw', 'go southwest', or 'go through the door'.")
+        elif "open" in choice:
+            print4 ("To open a door, just say 'open the door'. If you are unable to open the door, it might be because it is locked.")
+        elif any(elem in choice for elem in door_close):
+            print4 ("To close a door, you can say 'close', 'shut', or 'slam'. For example, say 'shut the door' to shut the door.")
+        elif any(elem in choice for elem in door_smash):
+            print4 ("To smash a door open, you can say 'smash', 'bash', 'destroy', or 'break'. For example, say 'smash the door open' to smash the door open. However, keep in mind that you need a sledgehammer to smash something, so make sure you've picked one up or mention it in your command, like 'smash the door open with the sledgehammer'.")
+        elif any(elem in choice for elem in door_unlock):
+            print4 ("To unlock a door, say 'unlock' or 'pick'. For example, say 'pick the lock' to unlock a door. Keep in mind that you need a lockpick to unlock something, so make sure you've picked one up or mention it in your command, like 'unlock the door with the lockpick'.")
+        elif "lock" in choice:
+            print4 ("To lock a door, just say 'lock the door'. Keep in mind that if you lock an open door and then shut it, you will have to unlock it to open it again. You also need a lockpick to lock something, so make sure you've picked one up or mention it in your command, like 'lock the door with the lockpick'.")
+        else:
+            print4 ("You can pick up or drop an object, smash or pick locks, open or shut doors, and move. For a specific list of commands, type 'help [subset]', like 'help pick up'.")
     # pick up item
     elif any(elem in choice for elem in add_inventory) or "pick" and "up" in choice:
         current_action.append("pickup")
-        if objectforsurroundings != '':
+        if objectforsurroundings != '' and len(inventory) == 0:
             inventory.append(objectforsurroundings)
             surroundings.remove(objectforsurroundings)
             print4 ("I've picked up a " + objectforsurroundings + ".")
+        elif objectforsurroundings != '' and len(inventory) != 0:
+            print4 ("I'm already holding the " + ''.join(inventory) + ".")
         else:
             print4 ("Sorry, I can't find anything like that around here.")
     # drop item
     elif any(elem in choice for elem in remove_inventory):
         current_action.append("drop")
-        if objectforinventory != '':
-            surroundings.append(objectforinventory)
-            inventory.remove(objectforinventory)
-            print4 ("I've dropped the " + objectforinventory + ".")
-        elif inventory == '':
+        if len(inventory) == 0:
             print4 ("I'm not holding anything.")
         elif len(inventory) != 0:
             print4 ("I've dropped the " + ''.join(inventory) + ".")
@@ -188,55 +247,106 @@ def action_seq():
             return "door"
         elif all(elem in choice for elem in travel):
             print4 ("Whoops! You forgot to tell me which way to go!")
+            next_room_sledge()
         else:
             print4 ("I don't understand. Please say something like 'go northeast' instead.")
+            next_room_sledge()
+    # unlock door
+    elif any(elem in choice for elem in door_unlock):
+        current_action.append("unlock")
+        if any(elem in surroundings for elem in unlocked_door):
+            print4 ("The door is already unlocked.")
+        elif "smashed door" in surroundings:
+            print4 ("The door is already broken open.")
+        elif any(elem in surroundings for elem in locked_door):
+            if "lockpick" in inventory:
+                unlock_door_cmd()
+            elif any(elem in choice for elem in ["lock", "door"]) and "lockpick" in choice:
+                if "lockpick" in surroundings:
+                    unlock_door_cmd()
+                else:
+                    print4 ("I can't see a lockpick anywhere near me.")
+            elif "lockpick" not in inventory:
+                print4 ("I don't have anything to pick the lock with.")
+            else:
+                print4 ("I don't understand.")
+    # lock door
+    elif "lock" in choice:
+        current_action.append("lock")
+        if any(elem in surroundings for elem in locked_door):
+            print4 ("The door is already locked.")
+        elif "smashed door" in surroundings:
+            print4 ("The door is broken open.")
+        elif any(elem in surroundings for elem in unlocked_door):
+            if "lockpick" in inventory:
+                lock_door_cmd()
+            elif any(elem in choice for elem in ["lock", "door"]) and "lockpick" in choice:
+                if "lockpick" in surroundings:
+                    lock_door_cmd()
+                else:
+                    print4 ("I can't see a lockpick anywhere near me.")
+            elif "lockpick" not in inventory:
+                print4 ("I don't have anything to pick the lock with.")
+            else:
+                print4 ("I don't understand.")
+        else:
+            print4 ("[[[FUCK]]]")
     # open door
     elif "open" in choice:
         current_action.append("open")
-        if "open door" in surroundings:
+        if any(elem in surroundings for elem in open_door):
             print4 ("The door is already open.")
         elif "smashed door" in surroundings:
             print4 ("The door is already broken open.")
-        elif len(inventory) == 0 and "closed door" in surroundings:
-            surroundings.append("open door")
-            surroundings.remove("closed door")
-            print4 ("I've opened the door.")
-        elif len(inventory) != 0 and "closed door" in surroundings:
-            print4 ("I can't open the door while I'm holding something.")
+        elif "locked closed door" in surroundings:
+            print4 ("The door is locked.")
+        elif "unlocked closed door" in surroundings:
+            if len(inventory) == 0:
+                surroundings.append("unlocked open door")
+                surroundings.remove("unlocked closed door")
+                print4 ("I've opened the door.")
+            else:
+                print4 ("I can't open the door while I'm holding something.")
         else:
             print4 ("Sorry, I can't see a door anywhere around here.")
     # close door
     elif any(elem in choice for elem in door_close):
         current_action.append("close")
-        if "closed door" in surroundings:
+        if any(elem in surroundings for elem in closed_door):
             print4 ("The door is already shut.")
         elif "smashed door" in surroundings:
             print4 ("I can't close a broken door.")
-        elif len(inventory) == 0 and "open door" in surroundings:
-            surroundings.append("closed door")
-            surroundings.remove("open door")
+        elif len(inventory) == 0 and "locked open door" in surroundings:
+            surroundings.append("locked closed door")
+            surroundings.remove("locked open door")
             print4 ("I've closed the door.")
-        elif len(inventory) != 0 and "open door" in surroundings:
+        elif len(inventory) == 0 and "unlocked open door" in surroundings:
+            surroundings.append("unlocked closed door")
+            surroundings.remove("unlocked open door")
+            print4 ("I've closed the door.")
+        elif len(inventory) != 0 and any(elem in surroundings for elem in open_door):
             print4 ("I can't shut the door while I'm holding something.")
         else:
             print4 ("Sorry, I can't see a door anywhere around here.")
     # smash door
     elif any(elem in choice for elem in door_smash):
         current_action.append("smash")
-        if "sledgehammer" not in inventory and "sledgehammer" not in choice:
+        if "smashed door" in surroundings:
+            print4 ("The door is already broken open.")
+        elif "sledgehammer" not in inventory and "sledgehammer" not in choice:
             print4 ("I don't have anything to smash the door with.")
-        elif "sledgehammer" in inventory or "sledgehammer" in choice and "closed door" or "open door" in surroundings:
-            if "closed door" in surroundings:
-                surroundings.remove("closed door")
-            elif "open door" in surroundings:
-                surroundings.remove("open door")
-            surroundings.append("smashed door")
-            print4 ("I've smashed the door.")
+        elif any(elem in surroundings for elem in smashable_door):
+            if "sledgehammer" in inventory:
+                smash_door_cmd()
+            elif "sledgehammer" in choice:
+                if "sledgehammer" in surroundings:
+                    smash_door_cmd()
+                else:
+                    print4 ("I can't see a sledgehammer anywhere near me.")
+            else:
+                print4 ("For some really weird reason I can't smash the door. Sorry.")
         else:
             print4 ("Sorry, I can't see a door anywhere around here.")
-    # help
-    elif any(elem in choice for elem in search_valid):
-        print4 ("This was supposed to be helpful. I hope it helped.")
     # no command
     else:
         print4 ("I don't understand.")
@@ -423,39 +533,46 @@ def next_room_sledge():
         holding = " holding a " + ''.join(inventory)
     else:
         holding = ''
-    if "closed door" in surroundings:
-        door_status = "closed door"
-    elif "open door" in surroundings:
-        door_status = "open door"
+    if "locked closed door" in surroundings:
+        door_status = " locked door"
+    elif "unlocked closed door" in surroundings:
+        door_status = " closed door"
+    elif any(elem in surroundings for elem in open_door):
+        door_status = "n open door"
     elif "smashed door" in surroundings:
-        door_status = "broken door"
-    if "sledgehammer" in surroundings:
+        door_status = " broken door"
+    if "sledgehammer" in surroundings and "lockpick" in surroundings:
+        near_me = " and a sledgehammer and a lockpick near me"
+    elif "sledgehammer" in surroundings:
         near_me = " and a sledgehammer near me"
+    elif "lockpick" in surroundings:
+        near_me = " and a lockpick near me"
     else:
         near_me = ''
-    print ("I'm" + holding + " in a room with a " + door_status + " to the north" + near_me + ".")
+    print ("I'm" + holding + " in a room with a" + door_status + " to the north" + near_me + ".")
     print1 ("What should I do?")
     answer_action = action_seq()
     if "move" in current_action:
         current_action.clear()
-        if any(elem in answer_action for elem in north) and 'closed door' in surroundings:
+        if any(elem in answer_action for elem in north) and any(elem in surroundings for elem in closed_door):
             print4 ("I can't go through a closed door.")
             next_room_sledge()
-        elif any(elem in answer_action for elem in north) and 'closed door' not in surroundings:
+        elif any(elem in answer_action for elem in north) and any(elem in surroundings for elem in walkable_door):
             print2 ("Moving north.")
             enter()
             rm_2()
-        elif "door" in answer_action and "closed door" not in surroundings:
+            print (answer_action)
+        elif "door" in answer_action and any(elem in surroundings for elem in walkable_door):
             print2 ("Going through the door.")
             enter()
             rm_2()
-        elif "door" in answer_action and "closed door" in surroundings:
+        elif "door" in answer_action and any(elem in surroundings for elem in closed_door):
             print4 ("I can't go through a closed door.")
             next_room_sledge()
         else:
             print4 ("There's a wall there. I can't go that way.")
             next_room_sledge()
-    elif "pickup" or "smash" or "drop" or "open" or "close" in current_action:
+    elif any(elem in current_action for elem in ["pickup", "drop", "lock", "unlock", "open", "close", "smash", "help"]):
         current_action.clear()
         next_room_sledge()
     else:
@@ -466,7 +583,8 @@ def rm_1_closed_door_a():
     inventory.clear()
     surroundings.clear()
     surroundings.append("sledgehammer")
-    surroundings.append("closed door")
+    surroundings.append("lockpick")
+    surroundings.append("locked closed door")
     next_room_sledge()
 
 # scripted room 2
@@ -489,3 +607,4 @@ def rm_2():
     game_over()
 
 pt_1()
+#rm_1_closed_door_a()
